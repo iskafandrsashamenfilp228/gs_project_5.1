c4ecc1beadcaad941357902a864639e12c886e11
iskafandrsashamenfilp228 <aleksandr.filippov0209@gmail.com>
2025-04-06 13:08:46 +0300

    carti

---
+++ html/main.js
@@ -0,0 +1,95 @@
+
+const iPhone = document.querySelector(".iPhone");
+
+const samsung = document.querySelector(".samsung")
+
+iPhone.addEventListener("click", function (event) {
+
+    let clickTarget = event.target;
+
+    let activeButton = iPhone.querySelector(".active");
+
+    let priceItem = iPhone.querySelector(".price");
+
+    let currentPrice;
+
+    let activeButtonColor = iPhone.querySelector("active-border");
+
+
+
+    if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains("active-border")) {
+
+        clickTarget.classList.add("active-border");
+
+        activeButtonColor.classList.remove("active-border");
+    }
+
+
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+
+        clickTarget.classList.add("active");
+
+        activeButtonColor.classList.remove("active");
+   
+
+        currentPrice = clickTarget.getAttribute("data-size");
+
+        if (currentPrice === "medium") {
+
+            priceItem.textContent = "Цена 80 000р.";
+        }
+
+        if (currentPrice === "large") {
+
+            priseItem.textContent = "Цена 100 000р.";
+        }
+    }
+})
+
+
+
+samsung.addEventListener("click", function (event) {
+
+    let clickTarget = event.target;
+
+    let activeButton = samsung.querySelector(".active");
+
+    let priceItem = samsung.querySelector(".price");
+
+    let currentPrice;
+
+    let activeButtonColor = samsung.querySelector("active-border");
+
+
+
+    if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains("active-border")) {
+
+        clickTarget.classList.add("active-border");
+
+        activeButtonColor.classList.remove("active-border");
+    }
+
+
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+
+        clickTarget.classList.add("active");
+
+        activeButtonColor.classList.remove("active");
+   
+
+        currentPrice = clickTarget.getAttribute("data-size");
+
+        if (currentPrice === "medium") {
+
+            priceItem.textContent = "Цена 80 000р.";
+        }
+
+        if (currentPrice === "large") {
+
+            priseItem.textContent = "Цена 100 000р.";
+        }
+    }
+})
+
